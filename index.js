b8a4ed582fe51af46ad52386e7da87cebeebd4d5
Kyler <kyler.b@live.com>
2021-11-04 11:54:50 -0500

    commit tree ae650de0b6d1ae9b3992658717bd6a1766b8a0f4

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(names, name) {
+    return names.filter(user => user.toLowerCase() == name.toLowerCase());
+}
+function fuzzyMatch(names, name) {
+    return names.filter(user => user.slice(0, name.length) === name);
+}
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
